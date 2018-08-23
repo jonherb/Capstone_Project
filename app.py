754b8e114129416b408a5aa71d76712cc4601c70
jonherb <jonathan.herberg@gmail.com>
2018-08-23 23:41:36 +0000

    adding home, about pages

--- app.py
+++ app.py
@@ -129,7 +129,7 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about', methods = ['GET', 'POST'])
+@app.route('/about', methods = ['GET'])
 def about():
     return render_template('about.html')
 
