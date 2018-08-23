9866deba907300313ead0d7e908a7948d7831553
jonherb <jonathan.herberg@gmail.com>
2018-08-23 04:13:40 +0000

    other html format trying

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ def make_output():
         png_output = StringIO()
         canvas.print_png(png_output)
         data = png_output.getvalue().encode('base64')
-        return '<img src="data:image/png;base64,{}">'.format(urllib.quote(data.rstrip('\n')))
+        return '<img src="data:image/png;base64,{}"/>'.format(urllib.quote(data.rstrip('\n')))
     
     
     wordcloud_figData = convert_fig_to_html(wordcloud_fig)
